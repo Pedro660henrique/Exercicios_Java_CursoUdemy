7483da29d3fd7373f70ce9bc18f048b5cee71af9
Pedro660henrique <pedro.santos660@hotmail.com>
2025-09-16 15:49:18 -0300

    Criação e conclusão do exercicio 2

--- Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
+++ Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Ex_ifElse {
 	
+	
+	//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
 	public static void Ex1() {
 		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 		
@@ -15,14 +17,34 @@ public class Ex_ifElse {
 		
 		if(n1 >= 0) {
 			System.out.println("Numero Positivo");
-		}else {
+		}
+		else {
 			System.out.println("Numero Negativo");
 		}
-		
+		sc.close();
 	}
+	
+	//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+	public static void Ex2() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		int n1;
+		
+		System.out.print("Digite um numero: ");
+		n1 = sc.nextInt();
+		
+		if(n1 % 2 == 0) {
+			System.out.println("Par");
+		}
+		else {
+			System.out.println("Impar");
+		}
+		sc.close();
+	}
+	
 
 	public static void main(String[] args) {
-		Ex1();
+		Ex2();
 	}
-
+	
 }
