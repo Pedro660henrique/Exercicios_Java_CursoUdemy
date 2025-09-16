ce69a7c755ed841f976f13a1caaa34d282946a5a
Pedro660henrique <pedro.santos660@hotmail.com>
2025-09-16 16:03:01 -0300

    Criação e conclusão do exercicio 3

--- Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
+++ Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
@@ -43,8 +43,31 @@ public class Ex_ifElse {
 	}
 	
 
+	/*Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+	Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+	ordem crescente ou decrescente.*/
+	public static void Ex3() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		int a, b;
+		
+		System.out.print("Insira o primeiro valor: ");
+		a = sc.nextInt();
+		System.out.print("Insira o segundo valor:: ");
+		b = sc.nextInt();
+		
+		if(a % b == 0 || b % a ==0) {
+			System.out.print("Sao Multiplos");
+		}else {
+			System.out.print("Nao sao Multiplos");
+		}
+		
+		sc.close();
+	}
+	
+
 	public static void main(String[] args) {
-		Ex2();
+		Ex3();
 	}
 	
 }
