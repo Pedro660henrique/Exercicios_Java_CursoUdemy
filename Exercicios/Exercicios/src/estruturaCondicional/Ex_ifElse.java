99bb7681f253bd242f43318cfe5de62eebb5e951
Pedro660henrique <pedro.santos660@hotmail.com>
2025-09-16 15:28:19 -0300

    Conclusão do exercicio 1

--- Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
+++ Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
@@ -1,10 +1,28 @@
 package estruturaCondicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ex_ifElse {
+	
+	public static void Ex1() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		int n1;
+		
+		System.out.print("Digite um numero: ");
+		n1 = sc.nextInt();
+		
+		if(n1 >= 0) {
+			System.out.println("Numero Positivo");
+		}else {
+			System.out.println("Numero Negativo");
+		}
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ex1();
 	}
 
 }
