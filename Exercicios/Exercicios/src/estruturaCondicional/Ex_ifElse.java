c1abf6650e71f2d5ca87ab48cff01d94425124c8
Pedro660henrique <pedro.santos660@hotmail.com>
2025-09-18 11:15:09 -0300

    Criação do codigo exercio 4

--- Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
+++ Exercicios/Exercicios/src/estruturaCondicional/Ex_ifElse.java
@@ -1,5 +1,8 @@
 package estruturaCondicional;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -65,9 +68,38 @@ public class Ex_ifElse {
 		sc.close();
 	}
 	
+	/*Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+	começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
+	public static void Ex4() {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.print("Digite o horario inicial (HH:mm): ");
+		LocalTime horaInicial = LocalTime.parse(sc.nextLine());
+		
+		System.out.print("Digite o horario Final (HH:mm): ");
+		LocalTime horaFinal = LocalTime.parse(sc.nextLine());
+		
+		
+		// Se o horário final for antes do inicial, soma 24h
+		if (horaFinal.isBefore(horaInicial)) {
+		    horaFinal = horaFinal.plusHours(24); 
+		}	
+		
+		//calcula a duração
+		Duration duracao = Duration.between(horaInicial, horaFinal);
+		
+		
+		
+		long horas = duracao.toHours();
+		long minutos = duracao.toMinutesPart();
+		
+		System.out.printf("Diferença:  %d:%02d%n", horas, minutos);
+		
+		sc.close();
+	}
 
 	public static void main(String[] args) {
-		Ex3();
+		Ex4();
 	}
 	
 }
